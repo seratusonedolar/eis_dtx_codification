09fbef84ac6002437874ea0187acf8a4098c6c13
seratusonedolar <seratusdolarone@gmail.com>
2024-10-30 11:48:22 +0700

    fanani_up1

--- application/views/eis/datatech_approval/index.php
+++ application/views/eis/datatech_approval/index.php
@@ -10,7 +10,7 @@ $this->load->section('scriptJS', $class_link . '/index_js', $dataJS);
 <!-- Default box -->
 <div class="card">
 	<div class="card-header">
-		<h3 class="card-title">Filter</h3>
+		<h3 class="card-title">Filtering</h3>
 
 		<div class="card-tools">
 			<button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
